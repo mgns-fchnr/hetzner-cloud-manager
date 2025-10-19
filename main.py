5e8a77b42273a1537a37e7a222153f13e3b8f73f
mgns-fchnr <magnus@mail-mgns.de>
2025-10-19 14:42:52 +0000

    added choice in the beginning

--- main.py
+++ main.py
@@ -12,7 +12,29 @@ api_key = os.getenv("API_KEY")
 
 client = Client(token=api_key)
 
-servers = client.servers.get_all()
+def status():
 
-for server in servers:
-    print(f"{server.name} {server.status}")
+    servers = client.servers.get_all()
+
+    for server in servers:
+        print(f"Server: {server.name} - Status: {server.status}")
+        print(f"+++++++++++++++++++++++++++++++++++++++++++++++")
+
+
+print(f"What do you want to do?")
+print(f"1 - See status of all servers")
+print(f"2 - Create new server (not available now)")
+print(f"3 - Exit")
+
+start = input("> ")
+
+if start == "1":
+    status()
+elif start == "2":
+    print("Feature not available now")
+elif start == "3":
+    exit
+else:
+    print("Invalid operation")
+
+
