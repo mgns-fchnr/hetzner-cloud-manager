76ace7dcd172c103ac4bd8d5fafa106469c6a482
mgns-fchnr <magnus@mail-mgns.de>
2025-10-19 14:50:35 +0000

    added start function to retry incase of error

--- main.py
+++ main.py
@@ -12,29 +12,37 @@ api_key = os.getenv("API_KEY")
 
 client = Client(token=api_key)
 
-def status():
+def start():
+    
+    print(f"What do you want to do?")
+    print(f"1 - See status of all servers")
+    print(f"2 - Create new server (not available now)")
+    print(f"3 - Exit")
 
+    choice = input("> ")
+
+    if choice == "1":
+        status()
+    elif choice == "2":
+        print("Feature not available now")
+    elif choice == "3":
+        exit
+    else:
+        print("Invalid operation")
+        start()
+
+def status():
+    
+    x = 0
     servers = client.servers.get_all()
 
     for server in servers:
+        if x == 0:
+            print(f"+++++++++++++++++++++++++++++++++++++++++++++++")
+
         print(f"Server: {server.name} - Status: {server.status}")
         print(f"+++++++++++++++++++++++++++++++++++++++++++++++")
+        
+        x += 1
 
-
-print(f"What do you want to do?")
-print(f"1 - See status of all servers")
-print(f"2 - Create new server (not available now)")
-print(f"3 - Exit")
-
-start = input("> ")
-
-if start == "1":
-    status()
-elif start == "2":
-    print("Feature not available now")
-elif start == "3":
-    exit
-else:
-    print("Invalid operation")
-
-
+start()
