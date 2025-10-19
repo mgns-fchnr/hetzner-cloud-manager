84b9f00975d42601c82882943a5eae7716379239
mgns-fchnr <magnus@mail-mgns.de>
2025-10-19 12:46:10 +0000

    first version which shows server name and status

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 #!venv/bin/python3
 
-print("hello world")
+from hcloud import Client
+from hcloud.images import Image
+from hcloud.server_types import ServerType
+
+client = Client(token="")
+
+servers = client.servers.get_all()
+
+for server in servers:
+    print(f"{server.name} {server.status}")
